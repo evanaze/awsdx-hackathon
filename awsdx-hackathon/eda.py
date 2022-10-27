597034cdab3629e3acd8483972315f4de8e0d2f4
Evan Azevedo <evan@azevedo.com>
2022-10-27 17:24:34 -0600

    working on eda

--- awsdx-hackathon/eda.py
+++ awsdx-hackathon/eda.py
@@ -3,9 +3,4 @@ import pandas as pd
 
 con = duckdb.connect()
 
-# Read in part of the
-chunksize = 10**6
-with pd.read_csv("data/opportunity_atlas.csv", chunksize=chunksize) as reader:
-    data_sample = pd.DataFrame(reader[0])
-
-print(data_sample.head())
+("SELECT * FROM read_csv_auto('data/opportunity_atlas.csv)'")
