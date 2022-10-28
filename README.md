8bba2d64820fbdf47aa3ef3ea3d3b3fd82f3c7a9
Evan Azevedo <evan@azevedo.com>
2022-10-27 18:07:26 -0600

    working with data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AWS Data Exchange Hackathon 2022
+
+Census tract to zip code provided by [HUD USPS](https://www.huduser.gov/portal/datasets/usps_crosswalk.html).
--- awsdx-hackathon/eda.py
+++ awsdx-hackathon/eda.py
@@ -3,4 +3,6 @@ import pandas as pd
 
 con = duckdb.connect()
 
-("SELECT * FROM read_csv_auto('data/opportunity_atlas.csv)'")
+# ("SELECT * FROM read_csv_auto('data/opportunity_atlas.csv)'")
+tract_to_zip = pd.read_excel("data/ZIP_TRACT_122021.xlsx")
+tract_to_zip.to_parquet("data/zip_to_tract_q42021.parquet")
