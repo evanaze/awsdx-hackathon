f27c6bb330cdf78b0d90014fcb34c50187993077
Evan Azevedo <evan@azevedo.com>
2022-10-29 11:27:43 -0600

    meltano init

--- awsdx-hackathon/eda.py
+++ awsdx-hackathon/eda.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 con = duckdb.connect()
 
-# ("SELECT * FROM read_csv_auto('data/opportunity_atlas.csv)'")
-tract_to_zip = pd.read_excel("data/ZIP_TRACT_122021.xlsx")
-tract_to_zip.to_parquet("data/zip_to_tract_q42021.parquet")
+opp_atlas = con.execute(
+    "SELECT * FROM read_csv_auto('data/opportunity_atlas.csv)'"
+).df()
