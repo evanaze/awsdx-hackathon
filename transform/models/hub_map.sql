472319a7649bebef35b7d143b65a633290efd0ed
Evan Azevedo <evan@azevedo.com>
2022-11-05 13:00:19 -0600

    working with data

--- transform/models/hub_map.sql
+++ transform/models/hub_map.sql
@@ -1,10 +1,14 @@
-CREATE TABLE HUB_MAP AS
+CREATE TABLE
+IF NOT EXISTS HUB_MAP AS
 SELECT *
 FROM STG_ZILLOW_IDX zill
     JOIN (SELECT zip_tract.zip, opp.kfr_rp_gp_pall as child_income
     FROM STG_OPP_ATLAS opp
         JOIN STG_ZIP_TRACT zip_tract
         ON opp.tract = zip_tract.tract) opp
-    ON opp.zip = zill.regionid;
+    ON opp.zip = zill.regionid
+    JOIN STG_LONG_LAT long
+    ON zill.regionid = long.zip;
 
 ALTER TABLE HUB_MAP DROP regionid;
+ALTER TABLE HUB_MAP DROP "zip:1";
