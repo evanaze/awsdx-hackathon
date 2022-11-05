493d0dd0d689bdbc05815fc17395fe71ba5f888f
Evan Azevedo <evan@azevedo.com>
2022-11-05 12:10:42 -0600

    working on superset

--- transform/models/hub_map.sql
+++ transform/models/hub_map.sql
@@ -1,17 +1,10 @@
-WITH
-    opportunity
-    AS
-    (
-        SELECT zip_tract.zip, opp.kfr_rp_gp_pall as child_income
-        FROM STG_OPP_ATLAS opp
-            JOIN STG_ZIP_TRACT zip_tract
-            ON opp.tract = zip_tract.tract
-    )
-
 CREATE TABLE HUB_MAP AS
 SELECT *
 FROM STG_ZILLOW_IDX zill
-    JOIN opportunity opp
+    JOIN (SELECT zip_tract.zip, opp.kfr_rp_gp_pall as child_income
+    FROM STG_OPP_ATLAS opp
+        JOIN STG_ZIP_TRACT zip_tract
+        ON opp.tract = zip_tract.tract) opp
     ON opp.zip = zill.regionid;
 
 ALTER TABLE HUB_MAP DROP regionid;
